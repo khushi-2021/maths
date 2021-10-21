a2e07aa512c4db3281d9b3729f46ee17268022a1
khushi-2021 <89191021+khushi-2021@users.noreply.github.com>
2021-10-21 12:17:22 +0530

    Delete script.js

--- script.js
+++
@@ -1,9 +0,0 @@
-const toggleDrop = (dropId) => {
-	const dropdown = document.getElementById(dropId);
-
-	if (dropdown.style.display == "block") {
-		dropdown.style.display = "none";
-	} else {
-		dropdown.style.display = "block";
-	}
-};
